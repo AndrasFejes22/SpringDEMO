f2e0294c5cceca7e4e2f7f8c7049c31d2a2beced
AndrasFejes22 <afejes22@gmail.com>
2023-02-07 20:25:48 +0100

    beans-only.xml updated PostService class changed (@Autowired constructor)

--- src/main/java/services/PostService.java
+++ src/main/java/services/PostService.java
@@ -3,17 +3,31 @@ package services;
 import model.Post;
 import model.User;
 import model.UserRole;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import services.publishers.PublisherService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PostService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PostService.class);
+
     private final UserService userService;
+
+    private final PublisherService publisherService;
     private final List<Post> posts = new ArrayList<>();
 
+    @Autowired
     public PostService(UserService userService) {
+        this(userService, null);
+    }
+
+    public PostService(UserService userService, PublisherService publisherService) {
         this.userService = userService;
+        this.publisherService = publisherService;
     }
 
     public void createPost(Post post) {
