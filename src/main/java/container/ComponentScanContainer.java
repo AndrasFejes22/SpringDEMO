aa37a1c17e5bd5a47f18f3367d3dfcba6246bf9c
AndrasFejes22 <afejes22@gmail.com>
2023-02-08 22:17:55 +0100

    ComponentScanContainer class added

---
+++ src/main/java/container/ComponentScanContainer.java
@@ -0,0 +1,23 @@
+package container;
+
+import model.Post;
+import model.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import services.PostService;
+import services.UserService;
+
+public class ComponentScanContainer {
+
+    public static void main(String[] args) {
+        User user = User.editor("testuser", "pw");
+        Post post = Post.of("Spring is fun", user);
+        ApplicationContext context = new ClassPathXmlApplicationContext("component-scan.xml");
+
+        UserService userService = context.getBean(UserService.class);
+        userService.registerUser(user);
+
+        PostService postService = context.getBean(PostService.class);
+        postService.createPost(post);
+    }
+}
